fd865015ae3d799795e7d98fc99bfffb7a149f1e
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-02 12:51:13 +0000

    ルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+ Route::get('/', function () {
+     return view('home');
+ })->name('home');
+
+ Route::get('/login', function() {
+     return view(('login'));
+ })->name('login');
+
+ Route::get('/signup', function() {
+     return view(('signup'));
+ })->name('signup');
+
+ Route::prefix('/todo')
+     ->as('todo.')
+     ->group(function () {
+
+         Route::get('/', function() {
+             return view('todo.index');
+         })->name('index');
+
+         Route::get('/new', function () {
+             return view('todo.new');
+         })->name('new');
+
+         Route::get('/edit', function () {
+             return view('todo.edit');
+         })->name('edit');
+     });
