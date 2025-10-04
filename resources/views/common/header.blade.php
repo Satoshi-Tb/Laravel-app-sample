bb303af8d0c8d2c55e18e689ba84d57c962a7478
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-04 05:28:19 +0000

    アイコン修正

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -11,7 +11,7 @@
                 <x-google-icon name="add" class="text-2xl"/>
             </x-link>
             <x-link href="{{ route('todo.index') }}">
-                <x-google-icon name="add" class="text-2xl"/>
+                <x-google-icon name="list" class="text-2xl"/>
             </x-link>
         </div>
     </div>
