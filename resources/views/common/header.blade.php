2365a54111a1a86eca3dd1bb2e628c2c640eb91a
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-05 10:05:04 +0000

    ヘッダーUI改善

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -8,15 +8,28 @@
 
         <div class="flex items-center gap-4">
             @auth
-                <span class="text-sm text-gray-600">{{ auth()->user()->name }}</span>
-            @endauth
+                <span class="px-3 py-1 text-xs font-semibold text-white bg-blue-500 rounded-full">
+                    {{ auth()->user()->name }} さんがログイン中
+                </span>
 
-            <x-link href="{{ route('todo.new') }}">
-                <x-google-icon name="add" class="text-2xl"/>
-            </x-link>
-            <x-link href="{{ route('todo.index') }}">
-                <x-google-icon name="list" class="text-2xl"/>
-            </x-link>
+                <x-link href="{{ route('todo.new') }}" class="flex items-center justify-center w-9 h-9 rounded-full bg-blue-50">
+                    <x-google-icon name="add" class="text-2xl"/>
+                </x-link>
+                <x-link href="{{ route('todo.index') }}" class="flex items-center justify-center w-9 h-9 rounded-full bg-blue-50">
+                    <x-google-icon name="list" class="text-2xl"/>
+                </x-link>
+            @else
+                <span class="px-3 py-1 text-xs font-semibold text-gray-600 bg-gray-100 rounded-full">
+                    未ログイン
+                </span>
+
+                <x-link href="{{ route('login') }}" class="px-3 py-1 text-sm font-semibold text-blue-600 border border-blue-400 rounded-full">
+                    ログイン
+                </x-link>
+                <x-link href="{{ route('signup') }}" class="px-3 py-1 text-sm font-semibold text-white bg-blue-500 rounded-full">
+                    新規登録
+                </x-link>
+            @endauth
         </div>
     </div>
 </header>
