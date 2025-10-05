0f90205666cfba5f4abf862a7a2889aae4201392
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-05 06:16:17 +0000

    Loginコントローラ実装

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function __invoke(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        $logined = Auth::attempt([
+            'email' => $data['email'],
+            'password' => $data['password'],
+        ]);
+
+        if ($logined === true) {
+            $request->session()->regenerate();
+            return redirect()->intended(route('todo.index'));
+        }
+
+        return back()->withErrors([
+            'login' => 'メールアドレスまたはパスワードが間違っています',
+        ]);
+    }
 }
