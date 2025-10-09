35d1724d6958e4548cf5993df521a9fd1740f412
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-09 13:43:05 +0000

    vite.configにtoggle.tsの追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,32 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/ts/app.ts'],
+            input: ["resources/ts/app.ts", "resources/ts/todo/toggle.ts"],
             refresh: true,
         }),
         tailwindcss(),
     ],
     server: {
-        host: '127.0.0.1',
+        host: "127.0.0.1",
         port: 5173,
         strictPort: true,
         hmr: {
-            protocol: 'ws'
+            protocol: "ws",
         },
         watch: {
             usePolling: true,
             interval: 1000,
-        }
+        },
     },
     resolve: {
         alias: {
-            '@': `${path.resolve(__dirname, 'resources/ts')}/`,
-            '@css': `${path.resolve(__dirname, 'resources/css')}/`,
-        }
-    }
+            "@": `${path.resolve(__dirname, "resources/ts")}/`,
+            "@css": `${path.resolve(__dirname, "resources/css")}/`,
+        },
+    },
 });
